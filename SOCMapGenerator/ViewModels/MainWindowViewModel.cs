9c72c666ecb90c8001f244d460c0ae6100b461c9
Jason <jason.shih.619@gmail.com>
2023-02-07 18:58:08 -0500

    Test Button Created

--- SOCMapGenerator/ViewModels/MainWindowViewModel.cs
+++ SOCMapGenerator/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace SOCMapGenerator.ViewModels
+﻿using System.ComponentModel;
+
+namespace SOCMapGenerator.ViewModels
 {
-	public class MainWindowViewModel : ViewModelBase
+	public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
 	{
-		public string Greeting => "Welcome to Avalonia!";
+		string _submitText = "Submit";
+		public new event PropertyChangedEventHandler PropertyChanged;
+
+		public string SubmitButton
+		{
+			get
+			{
+				return _submitText;
+			}
+			set
+			{
+				_submitText = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SubmitButton)));
+			}
+		}
+
+		public void SubmitClicked() => SubmitButton = "Test Run";
 	}
 }
