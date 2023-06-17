11189e3312c6f85b5aa5f3962a95bffb8f248527
Jason Shih <jasonshih.programming@gmail.com>
2023-06-17 09:50:38 -0400

    Created Map Generator Page

---
+++ SOCMapGenerator/SOCMapGenerator/Controllers/SOCController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SOCMapGenerator.Controllers
+{
+	public class SOCController : Controller
+	{
+		private readonly ILogger<SOCController> _logger;
+
+		public SOCController(ILogger<SOCController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
