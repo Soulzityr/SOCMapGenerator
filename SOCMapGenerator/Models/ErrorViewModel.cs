d62879502606a3312177551de0cbcef5713f6ea3
Jason <jason.shih.619@gmail.com>
2023-07-13 13:48:44 -0400

    Moved project structure

---
+++ SOCMapGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SOCMapGenerator.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
