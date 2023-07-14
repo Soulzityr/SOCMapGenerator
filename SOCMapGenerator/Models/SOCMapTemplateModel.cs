a00e60a80a900a5c7029600736b1724d61667692
Jason <jason.shih.619@gmail.com>
2023-07-13 20:59:55 -0400

    Small form creation

---
+++ SOCMapGenerator/Models/SOCMapTemplateModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SOCMapGenerator.Models
+{
+    public class SOCMapTemplateModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int PlayerCount { get; set; }
+    }
+
+    public class SOCMapTemplateCreate : SOCMapTemplateModel
+    {
+
+    }
+}
